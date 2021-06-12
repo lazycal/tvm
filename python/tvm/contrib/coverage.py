04928083530cdc435cc717d1589d5fc00d2ee498
ganler <jaway.liu@gmail.com>
2021-06-12 17:01:00 +0800

    feat: coverage hitmap content

--- python/tvm/contrib/coverage.py
+++ python/tvm/contrib/coverage.py
@@ -1,7 +1,12 @@
 import tvm
+import numpy as np
 
 total = tvm.get_global_func("tvm.contrib.coverage.total")
 now = tvm.get_global_func("tvm.contrib.coverage.now")
 # Because `tvm.contrib.coverage.now` relies on tvm's registry function, so after 
 # calling `reset`, the coverage will not be ZERO (but very small, e.g., 6).
 reset = tvm.get_global_func("tvm.contrib.coverage.reset")
+
+_hitmap_handler = tvm.get_global_func("tvm.contrib.coverage.hitmap")
+def hitmap():
+    return np.frombuffer(_hitmap_handler(), dtype=np.int8, count=total()) 
--- src/contrib/coverage/coverage.cc
+++ src/contrib/coverage/coverage.cc
@@ -1,11 +1,19 @@
 #include <tvm/runtime/registry.h>
 #include <memcov.hpp>
 
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.hitmap")
+    .set_body_typed([] {
+        TVMByteArray byte_arr{};
+        byte_arr.data = reinterpret_cast<const char*>(memcov::ptr());
+        byte_arr.size = memcov::total();
+        return byte_arr;
+    });
+
 TVM_REGISTER_GLOBAL("tvm.contrib.coverage.now")
     .set_body_typed([] () -> int64_t {return memcov::now();});
 
 TVM_REGISTER_GLOBAL("tvm.contrib.coverage.reset")
-    .set_body_typed([] () -> void { memcov::reset();});
+    .set_body_typed([] { memcov::reset();});
 
 TVM_REGISTER_GLOBAL("tvm.contrib.coverage.total")
     .set_body_typed([] () -> int64_t {return memcov::total();});
