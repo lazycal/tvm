4844b2370fa1cd5b962d4f40e5b293d414989d4c
ganler <jaway.liu@gmail.com>
2021-06-12 17:40:34 +0800

    feat: refact api and trasmit through strings

--- python/tvm/contrib/coverage.py
+++ python/tvm/contrib/coverage.py
@@ -1,12 +1,14 @@
 import tvm
-import numpy as np
 
-total = tvm.get_global_func("tvm.contrib.coverage.total")
-now = tvm.get_global_func("tvm.contrib.coverage.now")
 # Because `tvm.contrib.coverage.now` relies on tvm's registry function, so after 
 # calling `reset`, the coverage will not be ZERO (but very small, e.g., 6).
 reset = tvm.get_global_func("tvm.contrib.coverage.reset")
 
-_hitmap_handler = tvm.get_global_func("tvm.contrib.coverage.hitmap")
-def hitmap():
-    return np.frombuffer(_hitmap_handler(), dtype=np.int8, count=total()) 
+get_total = tvm.get_global_func("tvm.contrib.coverage.get_total")
+get_now = tvm.get_global_func("tvm.contrib.coverage.get_now")
+
+set_total = tvm.get_global_func("tvm.contrib.coverage.set_total")
+set_now = tvm.get_global_func("tvm.contrib.coverage.set_now")
+
+get_hitmap = tvm.get_global_func("tvm.contrib.coverage.get_hitmap")
+set_hitmap = tvm.get_global_func("tvm.contrib.coverage.set_hitmap")
--- src/contrib/coverage/coverage.cc
+++ src/contrib/coverage/coverage.cc
@@ -1,19 +1,31 @@
 #include <tvm/runtime/registry.h>
 #include <memcov.hpp>
 
-TVM_REGISTER_GLOBAL("tvm.contrib.coverage.hitmap")
-    .set_body_typed([] {
-        TVMByteArray byte_arr{};
-        byte_arr.data = reinterpret_cast<const char*>(memcov::ptr());
-        byte_arr.size = memcov::total();
-        return byte_arr;
-    });
-
-TVM_REGISTER_GLOBAL("tvm.contrib.coverage.now")
-    .set_body_typed([] () -> int64_t {return memcov::now();});
-
 TVM_REGISTER_GLOBAL("tvm.contrib.coverage.reset")
     .set_body_typed([] { memcov::reset();});
 
-TVM_REGISTER_GLOBAL("tvm.contrib.coverage.total")
-    .set_body_typed([] () -> int64_t {return memcov::total();});
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.get_hitmap")
+    .set_body_typed([] {
+        TVMByteArray arr;
+        arr.data = memcov::ptr();
+        arr.size = memcov::get_total();
+        return arr;
+    });
+
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.set_hitmap")
+    .set_body_typed([] (const std::string& src) {
+        char* dst = memcov::ptr();
+        std::memcpy(dst, src.c_str(), src.length());
+    });
+
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.set_now")
+    .set_body_typed([] (int64_t n) { memcov::set_now(n);});
+
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.get_now")
+    .set_body_typed([] () -> int64_t { return memcov::get_now();});
+
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.set_total")
+    .set_body_typed([] (int64_t t) { memcov::set_total(t);});
+
+TVM_REGISTER_GLOBAL("tvm.contrib.coverage.get_total")
+    .set_body_typed([] () -> int64_t { return memcov::get_total();});
